d677b3bf98411d5b4592425bc7967b4fec1a8467
Samuel Kong <kong.le@leximit.com>
2019-07-23 00:47:11 +0700

    fix bug checkout

---
+++ app/design/frontend/Lexim/infinity/Magento_Checkout/web/js/view/shipping-mixin.js
@@ -0,0 +1,132 @@
+/**
+ * Binary Anvil, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Binary Anvil, Inc. Software Agreement
+ * that is bundled with this package in the file LICENSE_BAS.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.binaryanvil.com/software/license/
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@binaryanvil.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this software to
+ * newer versions in the future. If you wish to customize this software for
+ * your needs please refer to http://www.binaryanvil.com/software for more
+ * information.
+ *
+ * @category    BinaryAnvil
+ * @package     Infinity
+ * @copyright   Copyright (c) 2018-2019 Binary Anvil,Inc. (http://www.binaryanvil.com)
+ * @license     http://www.binaryanvil.com/software/license
+ */
+
+define([
+    'jquery',
+    'underscore',
+    'Magento_Ui/js/form/form',
+    'ko',
+    'Magento_Customer/js/model/customer',
+    'Magento_Customer/js/model/address-list',
+    'Magento_Checkout/js/model/address-converter',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/action/create-shipping-address',
+    'Magento_Checkout/js/action/select-shipping-address',
+    'Magento_Checkout/js/model/shipping-rates-validator',
+    'Magento_Checkout/js/model/shipping-address/form-popup-state',
+    'Magento_Checkout/js/model/shipping-service',
+    'Magento_Checkout/js/action/select-shipping-method',
+    'Magento_Checkout/js/model/shipping-rate-registry',
+    'Magento_Checkout/js/action/set-shipping-information',
+    'Magento_Checkout/js/model/step-navigator',
+    'Magento_Ui/js/modal/modal',
+    'Magento_Checkout/js/model/checkout-data-resolver',
+    'Magento_Checkout/js/checkout-data',
+    'uiRegistry',
+    'mage/translate',
+    'Magento_Checkout/js/model/shipping-rate-service'
+], function (
+    $,
+    _,
+    Component,
+    ko,
+    customer,
+    addressList,
+    addressConverter,
+    quote,
+    createShippingAddress,
+    selectShippingAddress,
+    shippingRatesValidator,
+    formPopUpState,
+    shippingService,
+    selectShippingMethodAction,
+    rateRegistry,
+    setShippingInformationAction,
+    stepNavigator,
+    modal,
+    checkoutDataResolver,
+    checkoutData,
+    registry,
+    $t
+) {
+    'use strict';
+
+    var mixin = {
+
+        /**
+         * Save new shipping address
+         */
+        saveNewAdditionalAddress: function () {
+            var addressData,
+                newShippingAddress;
+
+            this.source.set('params.invalid', false);
+            this.triggerShippingDataValidateEvent();
+
+            if (!this.source.get('params.invalid')) {
+                addressData = this.source.get('shippingAddress');
+                // if user clicked the checkbox, its value is true or false. Need to convert.
+                addressData['save_in_address_book'] = this.saveInAddressBook ? 1 : 0;
+
+                // New address must be selected as a shipping address
+                newShippingAddress = createShippingAddress(addressData);
+                selectShippingAddress(newShippingAddress);
+                checkoutData.setSelectedShippingAddress(newShippingAddress.getKey());
+                checkoutData.setNewCustomerShippingAddress($.extend(true, {}, addressData));
+
+                this.isNewAddressAdded(true);
+
+                $('#add_shipping_new_address').trigger('click');
+            }
+        },
+
+        /**
+         * Cancel editing address
+         */
+        cancelEditNewAdditionalAddress: function () {
+            $('#add_shipping_new_address').trigger('click');
+        },
+
+        /**
+         * Set shipping information handler
+         */
+        setShippingInformation: function () {
+            if (this.validateShippingInformation()) {
+                setShippingInformationAction().done(
+                    function () {
+                        stepNavigator.next();
+                        $('[name="billing-address-same-as-shipping"]').trigger('click');
+                    }
+                );
+            }
+        }
+    };
+
+    return function (target) {
+        return target.extend(mixin);
+    };
+});
