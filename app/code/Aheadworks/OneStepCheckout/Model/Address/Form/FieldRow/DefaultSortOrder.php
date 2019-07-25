99905e4adb06fa7b7747913a07384a39d720e725
Samuel Kong <kong.le@leximit.com>
2019-07-25 14:03:25 +0700

    Checkout page - payment

--- app/code/Aheadworks/OneStepCheckout/Model/Address/Form/FieldRow/DefaultSortOrder.php
+++ app/code/Aheadworks/OneStepCheckout/Model/Address/Form/FieldRow/DefaultSortOrder.php
@@ -10,25 +10,25 @@ class DefaultSortOrder
     /**
      * @var array
      */
-    // private $defaultFieldRowsSortOrder = [
-    //     'name-field-row' => 0,
-    //     'address-field-row' => 1,
-    //     'city-field-row' => 2,
-    //     'country-region-zip-field-row' => 3,
-    //     'phone-company-field-row' => 4
-    // ];
+     private $defaultFieldRowsSortOrder = [
+         'name-field-row' => 0,
+         'address-field-row' => 1,
+         'city-field-row' => 2,
+         'country-region-zip-field-row' => 3,
+         'phone-company-field-row' => 4
+     ];
 
-    private $defaultFieldRowsSortOrder = [
-        'name-field-row' => 0,
-        'address-field-row' => 1,
-        'city-field-row' => 2,
-        'country-field-row' => 3,
-        'region-field-row' => 3,
-        'zip-field-row' => 3,
-        'phone-field-row' => 4,
-        'company-field-row' => 5,
-        'fax-field-row' => 6
-    ];
+//    private $defaultFieldRowsSortOrder = [
+//        'name-field-row' => 0,
+//        'address-field-row' => 1,
+//        'city-field-row' => 2,
+//        'country-field-row' => 3,
+//        'region-field-row' => 3,
+//        'zip-field-row' => 3,
+//        'phone-field-row' => 4,
+//        'company-field-row' => 5,
+//        'fax-field-row' => 6
+//    ];
 
     /**
      * Get default row sort order
