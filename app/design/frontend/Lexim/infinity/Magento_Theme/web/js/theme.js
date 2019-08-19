60b743ac420dd18bde8355a7ef636b9edd260a5f
Samuel Kong <kong.le@leximit.com>
2019-08-19 15:40:30 +0700

    remember me

--- app/design/frontend/Lexim/infinity/Magento_Theme/web/js/theme.js
+++ app/design/frontend/Lexim/infinity/Magento_Theme/web/js/theme.js
@@ -109,15 +109,5 @@ define([
         // });
         // }
 
-        // Click label and check/uncheck radio or checkbox
-        $('.checkbox-field label').click(function () {
-            let _this = $(this);
-            let _for = _this.prop('for');
-            let name = _this.attr('data-name');
-
-            if (_for && name) {
-                $('input[name="' + name + '"]').trigger('click');
-            }
-        });
     });
 });
