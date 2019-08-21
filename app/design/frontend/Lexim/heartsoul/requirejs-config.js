76d718b04cd0f38a9931c38db9067aa90e5cf9fe
Samuel Kong <kong.le@leximit.com>
2019-08-21 10:25:25 +0700

    hts home - instagram

--- app/design/frontend/Lexim/heartsoul/requirejs-config.js
+++ app/design/frontend/Lexim/heartsoul/requirejs-config.js
@@ -1,9 +1,13 @@
 var config = {
     paths: {
-        slick: 'js/slick.min'
+        slick: 'js/slick.min',
+        instafeed: 'Magento_Theme/js/instafeed.min'
     },
     shim: {
         slick: {
+            deps: ['jquery']
+        },
+        instafeed: {
             deps: ['jquery']
         }
     }
