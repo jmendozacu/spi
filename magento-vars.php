c2961c3850db18874f84ab2d46ae0361b00782cb
ubuntu <kong.le@leximit.com>
2019-09-02 15:02:20 +0000

    Implement multiple sites

--- magento-vars.php
+++ magento-vars.php
@@ -1,12 +1,4 @@
 <?php
-// enable, adjust and copy this code for each store you run
-// Store #0, default one
-//if (isHttpHost("example.com")) {
-//    $_SERVER["MAGE_RUN_CODE"] = "default";
-//    $_SERVER["MAGE_RUN_TYPE"] = "store";
-//}
-
-<?php
 function isHttpHost($host)
 {
     if (!isset($_SERVER['HTTP_HOST'])) {
@@ -15,10 +7,10 @@ function isHttpHost($host)
     return $_SERVER['HTTP_HOST'] ===  $host;
 }
 
-if (isHttpHost("heartsoulscrubs.lexim.net")) {
+if (isHttpHost("mcstaging.heartsoulscrubs.com")) {
     $_SERVER["MAGE_RUN_CODE"] = "hts";
     $_SERVER["MAGE_RUN_TYPE"] = "website";
-}elseif (isHttpHost("infinityscrubsv2.lexim.net")){
+}elseif (isHttpHost("mcstaging.infinityscrubs.com")){
     $_SERVER["MAGE_RUN_CODE"] = "base";
     $_SERVER["MAGE_RUN_TYPE"] = "website";
 }
